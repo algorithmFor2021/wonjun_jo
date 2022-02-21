21b44241392e933bc4f2fa3f56d2c32fb5a88332
hangeulisbest <wj100213@nate.com>
2022-02-21 23:16:26 +0900

    not solved

---
+++ boj/P10819/Main.java
@@ -0,0 +1,64 @@
+package boj.P10819;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static Fs fs = new Fs();
+    static int n;
+    static int cnt= 0;
+    static int[] arr = new int[10];
+    static int answer = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws Exception{
+        input();
+        dfs(0,new boolean[10],new ArrayList<>());
+        System.out.println(answer);
+        System.out.println(cnt);
+    }
+
+    static void input() throws Exception{
+        n = fs.nInt();
+        for(int i=0;i<n;i++){
+            arr[i] = fs.nInt();
+        }
+    }
+
+    static void dfs(int idx , boolean[] visit , ArrayList<Integer> list) {
+        if(idx == n) {
+            answer = Math.max(answer,cal(list));
+            cnt++;
+            return;
+        }
+
+        for(int i=0;i<n;i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                list.add(arr[i]);
+                dfs(idx+1,visit,list);
+                list.remove(Integer.valueOf(arr[i]));
+                visit[i] = false;
+            }
+        }
+    }
+
+    static int cal(ArrayList<Integer> list) {
+        int ret = 0;
+        for(int i=0;i<list.size()-1;i++) {
+            ret += Math.abs(list.get(i)-list.get(i+1));
+        }
+        return ret;
+    }
+
+    static class Fs{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        public int nInt() throws Exception{
+            if(!st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
+    }
+}
