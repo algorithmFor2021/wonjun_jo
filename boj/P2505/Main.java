f6523b049dae1b56574f1e82def10575cdaf6ae8
hangeulisbest <wj100213@nate.com>
2022-03-31 22:53:24 +0900

    두번뒤집기 실패

--- boj/P2505/Main.java
+++ boj/P2505/Main.java
@@ -3,7 +3,12 @@ package boj.P2505;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/**
+ * Main 설명 : 두번 뒤집기
+ * @author jowonjun
+ * @version 1.0.0
+ * 작성일 : 2022/03/31
+**/
 public class Main {
     static Fs fs = new Fs();
     static int n;
@@ -15,12 +20,13 @@ public class Main {
 
     }
 
-//    static boolean isOneReversed(int[] a) {
-//        int num = 0;
-//        for(int i=1;i<=n;i++) {
-//
-//        }
-//    }
+    static boolean isOneReversed(int[] a) {
+        int num = 0;
+        for(int i=1;i<=n;i++) {
+
+        }
+        return true;
+    }
 
     static boolean isRightCheck(int[] a) {
         for(int i=1;i<=n;i++) if(a[i]!=i) return false;
