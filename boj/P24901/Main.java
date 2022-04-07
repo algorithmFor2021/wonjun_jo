62b094ee01cfa59071cfc1ba27cd585eb3adcbd2
hangeulisbest <wj100213@nate.com>
2022-04-08 00:32:36 +0900

    무슨문제임

--- boj/P24901/Main.java
+++ boj/P24901/Main.java
@@ -1,2 +1,28 @@
-package boj.P24901;public class Main {
+package boj.P24901;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static Fs fs = new Fs();
+    public static void main(String[] args) throws Exception{
+        int n = fs.nInt();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<=n;i++){
+            String s = Integer.toBinaryString(i);
+            sb.append(s);
+        }
+        System.out.println(sb.toString());
+    }
+
+    static class Fs{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        public int nInt() throws Exception{
+            if(!st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+            return Integer.parseInt(st.nextToken());
+        }
+    }
 }
