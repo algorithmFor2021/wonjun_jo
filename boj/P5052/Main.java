d52f1d1e463911ba2948074cb73a5ac875869bd9
hangeulisbest <wj100213@nate.com>
2022-04-25 23:47:58 +0900

    주석

--- boj/P5052/Main.java
+++ boj/P5052/Main.java
@@ -5,7 +5,12 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.StringTokenizer;
-
+/**
+ * Main 설명 : 전화번호 목록
+ * @author jowonjun
+ * @version 1.0.0
+ * 작성일 : 2022/04/25
+**/
 public class Main {
     static Fs fs = new Fs();
 
