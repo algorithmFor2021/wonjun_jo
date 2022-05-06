83ef5ec345cbec209709b94d65acc0f00bb2e428
hangeulisbest <wj100213@nate.com>
2022-05-06 16:05:01 +0900

    dfs는 안되나?

--- boj/P1525/Main.java
+++ boj/P1525/Main.java
@@ -2,100 +2,55 @@ package boj.P1525;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Main {
     static Fs fs = new Fs();
-    static int[][] arr = new int[3][3];
-    static HashSet<String> visit = new HashSet<>();
+    static String str = "";
+    static HashMap<String,Integer> visit = new HashMap<>();
     static String cmp = "123456780";
-    static long answer = Long.MAX_VALUE;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
 
     public static void main(String[] args) throws Exception {
         for(int i=0;i<3;i++) {
             for(int j=0;j<3;j++) {
-                arr[i][j] = fs.nInt();
+                str += String.valueOf(fs.nInt());
             }
         }
 
-        dfs(0);
+        dfs(0,str);
 
-        System.out.println(visit.size());
 
-        List<String> collect = visit.stream().collect(Collectors.toList());
-        Collections.sort(collect);
-
-        for(String s : collect) {
-            System.out.println(s);
-        }
+        System.out.println(visit.get(cmp));
     }
 
-    static void dfs(int move) {
+    static void dfs(int move , String cur) {
 
-        String s = toStringHashCode();
-        if(visit.contains(s)) {
-            System.out.println(s);
-            return;
-        }
-
-        visit.add(s);
-        if(s.equals(cmp)) {
-            answer = Math.min(move,answer);
-            return;
-        }
-
-        if(move > 100) return;
-
-        int[] pos = findBlank();
+        int pos = cur.indexOf('0');
+        int x = pos / 3;
+        int y = pos % 3;
 
         for(int i=0;i<4;i++) {
-            int nx = pos[0] + dx[i];
-            int ny = pos[1] + dy[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-            if(checkRange(nx,ny)) {
-                swap(nx,ny,pos[0],pos[1]);
-                dfs(move + 1);
-                swap(nx,ny,pos[0],pos[1]);
-            }
-        }
+            if(nx >=0 && ny >=0 && nx <3 && ny < 3) {
+                int n = nx*3 + ny;
+                char ch = cur.charAt(n);
+                String next = cur;
+                next = next.replace('0','9');
+                next = next.replace(ch,'0');
+                next = next.replace('9',ch);
 
-    }
-
-    static boolean checkRange(int x,int y) {
-        return x>=0 && x<3 && y>=0 && y<3;
-    }
-
-    static void swap(int x,int y,int nx,int ny) {
-        int temp = arr[nx][ny];
-        arr[nx][ny] = arr[x][y];
-        arr[x][y] = temp;
-    }
-
-    static int[] findBlank() {
-        for(int i=0;i<3;i++) {
-            for(int j=0;j<3;j++) {
-                if(arr[i][j] == 0) {
-                    return new int[]{i,j};
+                if(!visit.containsKey(next)) {
+                    visit.put(next,move+1);
+                    dfs(move + 1, next);
                 }
             }
-        }
-        return new int[]{-1,-1};
-    }
 
-    static String toStringHashCode() {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<3;i++) {
-            for(int j=0;j<3;j++) {
-                sb.append(arr[i][j]);
-            }
         }
-        return sb.toString();
+
     }
 
     static class Fs{
