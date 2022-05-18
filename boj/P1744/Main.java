eb066e015b2a47549553f89628e3e815ab37b6f4
hangeulisbest <wj100213@nate.com>
2022-05-18 23:47:07 +0900

    수 묶기

---
+++ boj/P1744/Main.java
@@ -0,0 +1,16 @@
+package boj.P1744;
+/**
+ * Main 설명 : 수 묶기
+ * @author jowonjun
+ * @version 1.0.0
+ * 작성일 : 2022/05/18
+**/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    static class Fs{
+
+    }
+}
